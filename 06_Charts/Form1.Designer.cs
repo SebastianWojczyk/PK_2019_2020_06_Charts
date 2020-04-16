353b58a0b591c72a7f9354af687a7aad918c97cf
Sebastian Wojczyk <sebastian.wojczyk@wmii.uni.lodz.pl>
2020-04-16 10:18:45 +0200

    Prezentacja elementarnych możliwości komponentu Chart.

--- 06_Charts/Form1.Designer.cs
+++ 06_Charts/Form1.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(29, 36);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(555, 300);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
     }
 }
 
--- 06_Charts/Form1.cs
+++ 06_Charts/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace _06_Charts
 {
@@ -15,6 +16,18 @@ namespace _06_Charts
         public Form1()
         {
             InitializeComponent();
+
+            Series s = new Series();
+            s.Name = "My function";
+
+            s.ChartType = SeriesChartType.Line;
+
+            for (double x = -10; x <= 10; x += 0.1)
+            {
+                s.Points.AddXY(x, x * x);
+            }
+
+            chart.Series.Add(s);
         }
     }
 }
